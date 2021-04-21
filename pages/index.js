b1437262f10764363a3878748817fd7eea26dda0
bhaveek424 <61945842+bhaveek424@users.noreply.github.com>
2021-04-21 13:44:30 +0530

    'bug'

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home({ results }) {
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
 
-  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || request.fetchTrending.url}`).then(res => res.json());
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json());
 
   return {
     props: {
